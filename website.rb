d62bbc7c6508799d0cb4dc9d6ec97287ef7700cd
Joey Meyer <jmeyer41@gmail.com>
2015-05-31 11:55:26 +0800

    remove POST /students endpoint

--- website.rb
+++ website.rb
@@ -3,33 +3,6 @@ require 'kramdown'
 require './student_spreadsheet'
 require "google_drive"
 
-
-
-post '/students' do
-  
-  credentials = Credentials.new
-  session = GoogleDrive.login(credentials.email, credentials.password)
-
-  ws = session.spreadsheet_by_key("1dQDdmBeoWZhko29KNuKQHh2SddnXo-zILoukIG4KjgE").worksheets[0]
-
-  n = ws.rows.size+1
-
-  ws[n,1] = Time.now.to_s
-  ws[n,2] = params[:class_num]
-  ws[n,3] = params[:ssnum]
-  ws[n,4] = params[:chinese_name]
-  ws[n,5] = params[:english_name]
-  ws[n,6] = params[:email]
-
-  ws.save()
-  
-  return "Thank you for submitting. To return to the homepage, click <a href='http://sonia.meyercraft.net'>here</a>"
-  
-end
-
-
-
-
 post '/grades' do
   redirect to("/grades/#{params[:ssnum]}")
 end
